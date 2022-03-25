aede14cb4a2b6ac9446c13ab44fbaaf0c893cdbf
version bump <version-bumper@users.noreply.github.com>
2022-03-25 22:03:48 +0000

    Bump version: 0.15.0 → 0.16.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import re
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = '0.15.0'
+__version__ = '0.16.0'
 
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
